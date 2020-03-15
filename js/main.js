526aa43610e02ab05eae93c0f1dd9f701ed92fe8
Pranshul Saxena <32437942+pranshuls12@users.noreply.github.com>
2020-03-16 01:14:46 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js",
 	{"particles":
 	{"number":
-	{"value":96,"density":{"enable":true,"value_area":800}}
+	{"value":80,"density":{"enable":true,"value_area":800}}
 	,"color":{"value":"#ffffff"},"shape":{"type":"star","stroke":
 	{"width":0,"color":"#FF0000"},"polygon":{"nb_sides":5},"image":
 	{"src":"http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg","width":100,"height":100}},"opacity":
@@ -10,7 +10,7 @@ particlesJS("particles-js",
 	{"enable":true,"speed":6.409588744762158,"direction":"top-left","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":
     {"enable":true,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"window","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":
     {"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":200,"line_linked":{"opacity":1}},"bubble":
-    {"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":
+    {"distance":400,"size":40,"duration":2,"opacity":8,"speed":1},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":
     {"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 
  
